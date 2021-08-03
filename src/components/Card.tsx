df73536c03130ce1dba23470b19e99bbafb99d55
EmreYYZ <6238977+EmreYYZ@users.noreply.github.com>
2021-08-02 20:41:25 -0400

    Updated Card CSS to make it look more like an actual card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,10 +15,18 @@ interface IProps {
 
 export const Card: React.FC<IProps> = ({ card }) => {
   return (
-    <div className="inline-block w-28 h-36 my-2 border-4 border-black-300 rounded-lg cursor-pointer bg-black-50">
-      <p className={`text-2xl text-left m-1 font-bold ${card.isRed ? "text-scarlet-500" : "text-black-900"}`}>
-        <span className="block">{card.number}</span> <span className="block">{card.symbol}</span>
-      </p>
+    <div className="inline-block w-36 h-48 my-2 border-4 border-black-300 rounded-lg bg-black-50">
+      <div>
+        <p className={`text-2xl  ml-2 mt-1 text-left font-bold leading-none ${card.isRed ? "text-scarlet-500" : "text-black-900"}`}>
+          <span className="block">{card.number}</span> <span className="block">{card.symbol}</span>
+        </p>
+      </div>
+      <div className={` text-7xl text-center leading-none font-bold ${card.isRed ? "text-scarlet-500" : "text-black-900"}`}>{card.symbol}</div>
+      <div>
+        <p className={` transform rotate-180 text-2xl text-left mt-2 mr-2 font-bold leading-none ${card.isRed ? "text-scarlet-500" : "text-black-900"}`}>
+          <span className="block">{card.number}</span> <span className="block">{card.symbol}</span>
+        </p>
+      </div>
     </div>
   );
 };
